5d177ba02dd2873c7e1e3c73d2502a13c2baf353
florentqevani <flori030201@gmail.com>
2026-09-16 11:11:38 +0200

    feat: Update dependencies, modify navigation, and add devtools options

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,7 @@ import 'package:flutter/material.dart';
 
 import 'package:happy_pay_flutter/data/session.dart';
 import 'package:happy_pay_flutter/data/users.dart';
-import 'package:happy_pay_flutter/features/loan/loan_screen.dart';
-
+import 'package:happy_pay_flutter/features/splash_screen.dart';
 import 'features/onboarding/get_started_screen.dart';
 import 'features/onboarding/phone_number_screen.dart';
 import 'features/onboarding/otp_code.dart';
@@ -31,7 +30,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData().copyWith(scaffoldBackgroundColor: Colors.white),
 
       // home: const SplashScreen(),
-      home: LoanScreen(),
+      home: SplashScreen(),
       routes: {
         '/get_started': (context) => const GetStartedScreen(),
         '/phone_number': (context) => const PhoneNumberScreen(),
